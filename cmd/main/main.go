637ba95c47b00e789a4514ba2e9b136811bdce63
HuangXiaoL <718843907@qq.com>
2021-07-16 20:31:23 +0800

    输出

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"log"
 	"net/http"
+	"strings"
 )
 
+type ReturnData struct {
+	RequestAddress string `json:"request_address"`// 发起IP
+	RequestHeader string `json:"request_header"`//请求头
+}
 func main()  {
+	if err := http.ListenAndServe("0.0.0.0:8082", rout()); err != nil {
+		panic("Web服务初始化.....失败")
+	}
 
+}
+func rout() *chi.Mux {
 	r := chi.NewMux()
 
-	r.Get("/test/editor", requestOutput) //测试权限
+	r.Get("/", requestOutput)
+	return r
 }
+
 func requestOutput(w http.ResponseWriter, r *http.Request)  {
-	log.Println(w.Header())
-	log.Println(r.Header)
+	res:=ReturnData{}
+	res.RequestAddress=r.RemoteAddr
+
+	for k,v:=range r.Header{
+		res.RequestHeader=res.RequestHeader+k+"=>"+strings.Join(v, ", ")
+//log.Println(k,"=>",v)
+	}
+
+
+
 }
